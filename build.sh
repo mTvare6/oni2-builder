fb6d5ced9057ddcaca2f23c9af47f2141556a031
mTvare <57838468+mTvare6@users.noreply.github.com>
2021-03-26 11:14:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 npm install -g esy
-brew install cmake libpng ragel libtool gettext
+brew install cmake libpng ragel libtool gettext git npm
+PATH='usr/local/bin:$PATH'
+ulimit -Sn 4096 
+
 git clone https://github.com/onivim/oni2
 cd oni2
 npm install -g node-gyp
 node install-node-deps.js
-ulimit -Sn 4096 
 esy install
 esy bootstrap
 esy build
-esy '@release' run -f --checkhealth
+esy run -f --checkhealth
+
 esy '@release' install
+esy '@release' run -f --checkhealth
 esy '@release' run --help
 esy '@release' create
 
