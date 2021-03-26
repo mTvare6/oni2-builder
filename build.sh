704a5cec387ba3eb8b5357c92494cfd3ef80c009
mTvare <57838468+mTvare6@users.noreply.github.com>
2021-03-26 13:54:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,10 +11,8 @@ node install-node-deps.js
 esy install
 esy bootstrap
 esy build
-esy run -f --checkhealth
 
 esy '@release' install
-esy '@release' run -f --checkhealth
 esy '@release' run --help
 esy '@release' create
 
